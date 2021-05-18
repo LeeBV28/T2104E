5023853f5afd7b436642bc2833dcd0c2ab1c60fb
LeeBV28 <84066230+LeeBV28@users.noreply.github.com>
2021-05-18 16:15:49 +0700

    them bai 3.2

---
+++ Assignment 3/as3.2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int n=4000;
+	n=(n*0.08)+n;
+	n=(n*0.08)+n;
+	n=(n*0.08)+n;
+	n=(n*0.08)+n;
+	printf("So tien co duoc khi gui ngan hang 4000$ voi lai suat 0.8%/nam va tinh lai kep la ");	
+	printf("%d$",n);
+	
+}
